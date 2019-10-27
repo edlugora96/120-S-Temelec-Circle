1550853fc462fc77e30216fd2ced56377beb016f
Eduardo Gonzalez <edlu.gora96@gmail.com>
2019-10-27 13:24:11 -0400

    start new project

--- components/Loading.js
+++ components/Loading.js
@@ -1,1 +1,26 @@
-Loading.js;
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles(theme => ({
+  section: {
+    display: "flex",
+    width: "100vw",
+    height: "100vh",
+    alignItems: "center",
+    justifyContent: "center",
+    backgrounColor: "#000"
+  }
+}));
+
+const Loading = props => {
+  const classes = useStyles();
+  return (
+    <section className={classes.section}>
+      <img
+        src="https://propertysonoma.com/media/logow-ddb9627d7439acdf7dd44996c1134ee6.png"
+        alt="KP Loadding..."
+      />
+    </section>
+  );
+};
+
+export default Loading;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import IndexView from "../indexView";
 import connect from "$redux/connect";
+import Loading from "$react/Loading";
 import { withRouter } from "next/router";
 class Index extends React.Component {
   render() {
-    return <IndexView {...this.props} />;
+    return <Loading {...this.props} />;
   }
 }
 
